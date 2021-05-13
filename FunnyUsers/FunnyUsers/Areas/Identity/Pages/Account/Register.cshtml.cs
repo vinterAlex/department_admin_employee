5b219a8c857cf3bdbca8e0e60e7c8ac76170b25b
vinteralex <alex.vinter24@gmail.com>
2021-05-13 15:19:23 +0300

    minor changes

--- FunnyUsers/FunnyUsers/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ FunnyUsers/FunnyUsers/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -43,42 +43,13 @@ namespace FunnyUsers.Areas.Identity.Pages.Account
         }
 
         [BindProperty]
-        public InputModel Input { get; set; }
+        public CreateUserModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
 
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        public class InputModel
-        {
 
-            [Required]
-            [Display(Name = "Name")]
-            public string Name { get; set; }
-
-            [Key]
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
-
-           
-            [Display(Name = "Department")]
-            public string Department { get; set; }
-
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
-
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-
-            public string Role { get; set; }
-        }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
