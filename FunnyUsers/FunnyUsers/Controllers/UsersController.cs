422e939fc963711de2099117a5a7f647ade6f9cd
vinteralex <alex.vinter24@gmail.com>
2021-05-13 15:04:10 +0300

    as a backup

--- FunnyUsers/FunnyUsers/Controllers/UsersController.cs
+++ FunnyUsers/FunnyUsers/Controllers/UsersController.cs
@@ -21,90 +21,75 @@ namespace FunnyUsers.Controllers
         //ApplicationUser _appUser;
 
 
-          public UsersController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager, ApplicationDbContext db,RoleManager<IdentityRole> roleManager)
+        public UsersController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _db = db;
             _roleManager = roleManager;
         }
-        //public UsersController(UserManager<IdentityUser> userManager,ApplicationDbContext db, RoleManager<IdentityRole> roleManager,ApplicationUser appUser)
-        //{
-        //    _userManager = userManager;
-        //    _db = db;
-        //    _roleManager = roleManager;
-        //    //_appUser = appUser;
-        //    
-        //}
 
-        
+
+
 
 
 
 
         public IActionResult Index()
         {
-            return View(_db.ApplicationUsers.ToList());
+            return View(_db.ApplicationUsers.ToList()); //works fine
+            //return View(_userManager.Users.ToList());
         }
 
-      
+
+        public async Task<ActionResult> Create()
+        {
+            return View();
 
 
-  
+        }
+
+
+
 
 
         public IActionResult Edit(string userID) // need to make it string due to IdentityUser<string>
         {
-            
+
             var user = _db.ApplicationUsers.Where(u => u.Id == userID).FirstOrDefault();
+           // var user = _db.ApplicationUsers.ToList();
             return View(user);
         }
 
-
-
-
-
-
-        
-        public async Task<ActionResult> Create()
+        [HttpPost]
+        public async Task<ActionResult> Edit(ApplicationUser userModel, string id)
         {
-            return View();
-            //return View("Identity/Account/Register");
 
+            return RedirectToAction("Index");
         }
+
+        //public async Task<ActionResult> DeleteUser(string id)
+        //{
+        //    var user = _db.ApplicationUsers.Where(u => u.Id == id).FirstOrDefault();
+        //    return RedirectToAction();
+        //}
 
         [HttpPost]
-        public IActionResult Edit(ApplicationUser userModel)
+        public async Task<ActionResult>DeleteUser(string userId)
         {
-            //IList<IdentityRole> listOfRoles = new List<IdentityRole>();
-            
-        
-        
-            var userToEdit = _db.ApplicationUsers.Where(us => us.Id == userModel.Id).FirstOrDefault();
-        
-            //foreach(var roles in _roleManager.Roles.ToList())
-            //{
-            //    listOfRoles.Add(roles);
-            //}
-        
-            //var currentRole = _userManager.GetRolesAsync(userToEdit);
-        
-            userToEdit.UserName = userModel.UserName;
-            userToEdit.Email = userModel.Email;
-            userToEdit.Department = userModel.Department;
-            userToEdit.Role = userModel.Role;
-        
-            //_userManager.RemoveFromRolesAsync(userToEdit, currentRole.Result.ToList());
-            //_userManager.AddToRoleAsync(userToEdit, role.Result.Name);
-            
-        
-            var result = _db.Update(userToEdit);
-        
-            ViewData["roles"] = _roleManager.Roles.ToList();
-            return View(userToEdit);
+            var user = _db.ApplicationUsers.ToList().FirstOrDefault(); //works but it deletes only the first one...
+            //var user = _db.ApplicationUsers.ToList().FirstOrDefault(x=>x.Id==userId);//.Where(u => u.Id == userId);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            return View("Index");
+
+
         }
-
-
 
     }
 }
